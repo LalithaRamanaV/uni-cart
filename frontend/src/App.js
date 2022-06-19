5eb398700d842052da23cea9483df36576fd1e89
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-20 00:08:33 +0530

    feat: created products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -6,7 +7,26 @@ function App() {
       <header>
       <a href="/">UniCart</a>
       </header>
-      <main>products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+        {
+          data.products.map(product => (<div className="product"key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name}/>
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+            <p><strong>INR {product.price}</strong></p>
+            <button>Add to cart</button>
+            </div>
+          </div>))
+        }
+        </div>
+      
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: 'Women',
+            slug: 'Women',
+            category: 'Daniel Willington',
+            image:'/images/image1.png',
+            price: 2000,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'High quality'
+        },
+        {
+            name: 'Watch',
+            slug: 'Watch',
+            category: 'Watches',
+            image:'/images/image1.png',
+            price: 2000,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'High quality'
+        },
+        {
+            name: 'watch-women',
+            slug: 'watch-women',
+            category: 'Watches',
+            image:'/images/image1.png',
+            price: 2000,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'High quality'
+        },
+        {
+            name: 'Women Watch',
+            slug: 'Women Watch',
+            category: 'Daniel Willington',
+            image:'/images/image1.png',
+            price: 2000,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'High quality'
+        }
+    ]
+}
+export default data;
