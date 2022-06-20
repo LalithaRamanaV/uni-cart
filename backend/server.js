12391e5b10c2271858bd1b3b2b55a6671ad8e4d7
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-21 00:01:52 +0530

    build: created sign in backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -16,30 +17,18 @@ mongoose.connect(process.env.MONGODB_URI)
 })
 const app = express();
 
-// app.get("/api/products", (req,res) =>{
-//     res.send(data.products );
-// })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.get("/api/products/slug/:slug", (req,res) =>{
-//     const product = data.products.find(x => x.slug === req.params.slug);
-//     if(product) {
-//         res.send(product);
-//     } else {
-//         res.status(404).send({message: "Product Not Found"});
-//     }
-// })
-
-// app.get('/api/products/:id', (req, res) => {
-//     const product = data.products.find((x) => x._id === req.params.id);
-//     if (product) {
-//       res.send(product);
-//     } else {
-//       res.status(404).send({ message: 'Product Not Found' });
-//     }
-//   });
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
