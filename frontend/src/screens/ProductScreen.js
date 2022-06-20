926aea2237e908b1afa3babcc7a7a28c7b137ec6
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-20 18:50:19 +0530

    feat: created loading component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,6 +10,9 @@ import Card from "react-bootstrap/Card"
 import Badge from "react-bootstrap/Badge"
 import Button from "react-bootstrap/Button"
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -41,17 +44,18 @@ const [{ loading,error,product },dispatch] = useReducer(reducer,{
       const result = await axios.get(`/api/products/slug/${slug}`);
       dispatch({ type: 'FETCH_SUCCESS', payload: result.data});
       } catch(err) {
-       dispatch({ type: 'FETCH_FAIL', payload: err.message });
+       dispatch({ type: 'FETCH_FAIL', payload: getError(err)});
       }
      
     };
     fetchData();
   }, [slug]);
-  return loading? (
-  <div>loading...</div>
-  ) : error? (
-  <div>{error}</div>
-  ) : (
+
+  return loading ? (
+           <LoadingBox />
+           ) : error ? (
+             <MessageBox variant="danger">{error}</MessageBox>
+           ) : (
    <div>
     <Row>
        <Col md={6}>
