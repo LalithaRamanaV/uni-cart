054842dc79b46870470214c3df9e4f795fbc6f7e
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-20 20:04:04 +0530

    build: create-react-context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter,Link, Route, Routes} from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
 import {LinkContainer} from "react-router-bootstrap"
+import {useContext} from "react";
+import {Store} from "./Store";
+import Nav from "react-bootstrap/Nav";
+import Badge from "react-bootstrap/Badge"
 
 function App() {
+
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
     <div className="d-flex flex-column site-container">
@@ -16,6 +24,17 @@ function App() {
           <LinkContainer to="/">
             <Navbar.Brand>UniCart</Navbar.Brand>
           </LinkContainer>
+
+          <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
         </Container>
         </Navbar>
       </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import {HelmetProvider} from "react-helmet-async"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css';
 import App from './App';
+import { StoreProvider } from './Store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StoreProvider>
     <HelmetProvider>
     <App />
     </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
 
