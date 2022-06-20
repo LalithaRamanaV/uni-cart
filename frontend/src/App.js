634ae5571913b2f3301478784aded8c3c5cdf551
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-21 02:18:54 +0530

    build: payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
   };
   return (
     <BrowserRouter>
@@ -82,6 +84,7 @@ function App() {
                 path="/shipping"
                 element={<ShippingAddressScreen />}
               ></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -13,6 +13,9 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
     ? JSON.parse(localStorage.getItem('shippingAddress'))
     : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+    ? localStorage.getItem('paymentMethod')
+    : '',
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -50,6 +53,7 @@ function reducer(state, action) {
           cart: {
             cartItems: [],
             shippingAddress: {},
+            paymentMethod: '',
           },
         };
       case 'SAVE_SHIPPING_ADDRESS':
@@ -60,6 +64,12 @@ function reducer(state, action) {
             shippingAddress: action.payload,
           },
         };
+
+        case 'SAVE_PAYMENT_METHOD':
+          return {
+            ...state,
+            cart: { ...state.cart, paymentMethod: action.payload },
+          };
 
       default:
       return state;
