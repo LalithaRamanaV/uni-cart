91905f9dd640f07f67362c8b363993976a4595f3
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-20 22:01:21 +0530

    build: connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.log(err.message);
+})
 const app = express();
 
 app.get("/api/products", (req,res) =>{
