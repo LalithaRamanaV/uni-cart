36e720322eb0b09260594ed436082710184ca598
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-21 00:48:41 +0530

    build: sign in screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,39 @@
-
-import {BrowserRouter,Link, Route, Routes} from "react-router-dom"
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import Navbar from "react-bootstrap/Navbar"
-import Container from "react-bootstrap/Container"
-import {LinkContainer} from "react-router-bootstrap"
-import {useContext} from "react";
-import {Store} from "./Store";
-import Nav from "react-bootstrap/Nav";
-import Badge from "react-bootstrap/Badge"
-import CartScreen from "./screens/CartScreen";
-import SigninScreen from "./screens/SigninScreen";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
 
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
 
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
   return (
     <BrowserRouter>
-    <div className="d-flex flex-column site-container">
-      <header>
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>UniCart</Navbar.Brand>
-          </LinkContainer>
-
-          <Nav className="me-auto">
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>UniCart</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
@@ -36,26 +42,47 @@ function App() {
                     </Badge>
                   )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
               </Nav>
-        </Container>
-        </Navbar>
-      </header>
-      <main>
-        <Container className="mt-3">
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>}/>
-          <Route path="/cart" element={<CartScreen/>}/>
-          <Route path="/signin" element={<SigninScreen/>}/>
-          <Route path="/" element={<HomeScreen/>}/>
-        </Routes>
-        </Container>
-      </main>
-      <footer>
-        <div className="text-center">All rights reserved</div>
-      </footer>
-    </div>
+              </Container>
+              </Navbar>
+              </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
-
 export default App;
