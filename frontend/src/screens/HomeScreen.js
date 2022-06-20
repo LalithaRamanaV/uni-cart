4aac4dab44f0d971c0b011f527cbef79f5fef22a
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-20 09:24:19 +0530

    build: updated pubilc

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,42 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState} from 'react'
 import { Link } from 'react-router-dom'
 import axios from "axios"
+import logger from "use-reducer-logger"
 // import data from '../data'
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading:true};
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading:false};
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error:action.payload};
+    default:
+       return state;
+  }
+}
+
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const [{ loading,error,products },dispatch] = useReducer(logger(reducer),{
+    products:[],
+    loading:true,
+    error:'',
+  });
+  // const [products, setProducts] = useReducer([]);
   useEffect(()=> {
-    const fetchData = async() => {
+    const fetchData = async () => {
+      dispatch({ type:'FETCH_REQUEST' });
+      try {
       const result = await axios.get("/api/products");
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_SUCCESS', payload: result.data});
+      } catch(err) {
+       dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      // setProducts(result.data);
     };
+
     fetchData();
   }, []);
   return (
