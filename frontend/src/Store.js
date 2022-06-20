c367ec37d0b71d89bc168bb756a3e197e1105d9c
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-21 02:46:08 +0530

    build: create place order action

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -44,6 +44,10 @@ function reducer(state, action) {
         return { ...state, cart: { ...state.cart, cartItems } };
       }
 
+      case 'CART_CLEAR':
+        return { ...state, cart: { ...state.cart, cartItems: [] } };
+  
+
       case 'USER_SIGNIN':
         return { ...state, userInfo: action.payload };
       case 'USER_SIGNOUT':
