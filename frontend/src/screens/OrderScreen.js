f58cc839f78bacc0d7054a050b92082c38c81d3d
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-22 01:02:23 +0530

    build: update order screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -120,7 +120,7 @@ export default function OrderScreen() {
           type: 'resetOptions',
           value: {
             'client-id': clientId,
-            currency: 'INR',
+            currency: 'USD',
           },
         });
         paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
